06d1741fd79f9803878f0eb8fdf6a7b8d51b0f6c
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-08-17 14:37:46 +0530

    Javascript : Question 1

---
+++ javascript/q1.js
@@ -0,0 +1,11 @@
+// question one
+// What will be the output of the following code?
+
+let a = [1, 2, 3];
+a[5] = 6;
+console.log(a.length); //4
+console.log(a); //[1,2,3,6]
+
+// output
+//? 6
+//? [ 1, 2, 3, <2 empty items>, 6 ]
